6784ce7caff5d83b5c78ad7af75d86d68c71857b
Maria Canals <mariacanals23@gmail.com>
2021-07-08 20:30:55 +0200

    favicon changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../src/LogoMC.png';
+import Logo from '../../src/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
